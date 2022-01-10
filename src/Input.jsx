429eb82475bc28effa919e2d5417e346d52359b3
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2022-01-10 16:30:35 +0100

    initial commit

---
+++ src/Input.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  FormErrorMessage,
+  FormControl,
+  FormLabel,
+  Input as TextInput,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+
+const Input = ({ label, inputRef, mb, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <FormControl mb={mb || 8} isRequired isInvalid={meta.touched && meta.error}>
+      {label && (
+        <FormLabel fontSize="md" htmlFor={props.id || props.name}>
+          {label}
+        </FormLabel>
+      )}
+      <TextInput ref={inputRef} {...field} {...props} borderRadius="0" />
+      {meta.touched && meta.error ? (
+        <FormErrorMessage>{meta.error}</FormErrorMessage>
+      ) : null}
+    </FormControl>
+  );
+};
+
+export default Input;
